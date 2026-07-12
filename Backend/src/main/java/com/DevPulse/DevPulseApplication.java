0995e4c8cc968deb15a0314bac7b621d62bcc0bf
Karthik <karthiksaini7800@gmail.com>
2026-07-12 21:11:49 +0530

    Backend init

---
+++ Backend/src/main/java/com/DevPulse/DevPulseApplication.java
@@ -0,0 +1,14 @@
+package com.DevPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPulseApplication.class, args);
+		System.out.println("Working");
+	}
+
+}
---
+++ Backend/src/main/java/com/DevPulse/entity/User.java
@@ -0,0 +1,10 @@
+package com.DevPulse.entity;
+
+import org.bson.types.ObjectId;
+
+public class User {
+
+    private ObjectId id;
+    private String name;
+    private String password;
+}
